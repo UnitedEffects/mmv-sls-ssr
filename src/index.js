acb33d4ca9d95c12dca0e564cbd5a0c08f511d38
Bo Motlagh <borzou@theboeffect.com>
2019-10-24 19:44:36 -0400

    code work but puppeteer installs chrome which makes this package huge... this is a problem for lambda. still brainstorming on a fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import "@babel/polyfill"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import sls from 'serverless-http';
 import express from 'express';
 import proxy from 'express-http-proxy';
